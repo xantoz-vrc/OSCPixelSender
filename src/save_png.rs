98be025d73e7d6917ee3f5d98e13aaf3a51e841b
xantoz <xantoz@nowhere.com>
2024-12-29 21:03:55 +0100

    save_png: Set Compression::Best when saving PNG files
    Will this improve their compression straight out of the gate? I
    noticed that with whatever the default was we were quite far away from
    what optipng could achieve.

--- src/save_png.rs
+++ src/save_png.rs
@@ -101,6 +101,7 @@ pub fn save_png(
     };
     encoder.set_color(typ);
     encoder.set_depth(bitdepth);
+    encoder.set_compression(png::Compression::Best);
 
     println!("Saving PNG of color {typ:?} with bit depth {bitdepth:?}");
 
