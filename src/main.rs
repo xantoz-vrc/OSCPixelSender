e683d145c6ea8a293a14071a0348de7e17adade6
xantoz <xantoz@nowhere.com>
2024-12-29 21:03:33 +0100

    Add a nice little macro for getting function names
    TODO: This should be moved to another file really, shouldn't it?

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,18 @@ use std::sync::RwLock;
 use std::sync::Mutex;
 use std::thread;
 
+macro_rules! function {
+    () => {{
+        fn f() {}
+        fn type_name_of<T>(_: T) -> &'static str {
+            std::any::type_name::<T>()
+        }
+        let name = type_name_of(f);
+        name.strip_suffix("::f").unwrap()
+    }}
+}
+
+
 fn get_file() -> Option<PathBuf> {
     let mut nfc = dialog::NativeFileChooser::new(dialog::FileDialogType::BrowseFile);
 
