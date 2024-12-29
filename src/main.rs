25e2b98ab93741f85d506e9e68edbe40aea208d8
xantoz <xantoz@nowhere.com>
2024-12-29 21:03:29 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-
-/*
-use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
-
-fn main() {
-    let app = app::App::default();
-    let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
-    let mut frame = Frame::new(0, 0, 400, 200, "");
-    let mut but = Button::new(160, 210, 80, 40, "Click me!");
-    wind.end();
-    wind.show();
-    but.set_callback(move |_| frame.set_label("Hello World!")); // the closure capture is mutable borrow to our button
-    app.run().unwrap();
-}
-*/
-
 use fltk::{app, frame::Frame, enums::FrameType, image::SharedImage, prelude::*, window::Window, group::*, button::Button, dialog};
 use std::error::Error;
 use std::rc::Rc;
@@ -118,17 +102,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         });
     }
 
-/*
-    let mut image = SharedImage::load("F:/tw20230603-1.jpg")?;
-    image.scale(512, 512, true, true);
-
-    frame.borrow_mut().set_image(Some(image));
-*/
-/*
-    // // To remove an image
-    // frame.borrow_mut().set_image(None::<SharedImage>);
-*/
-
     col.end();
     row.end();
     wind.end();
@@ -141,44 +114,3 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("App finished");
     Ok(())
 }
-
-/*
-use fltk::{prelude::*, *};
-fn main() {
-    let app = app::App::default();
-    let mut win = window::Window::default().with_size(900, 300);
-    let mut chooser = dialog::FileChooser::new(
-        ".",                    // directory
-        "*",                    // filter or pattern
-        dialog::FileChooserType::Multi, // chooser type
-        "Title Of Chooser",     // title
-    );
-    chooser.show();
-    chooser.window().set_pos(300, 300);
-    // Block until user picks something.
-    //     (The other way to do this is to use a callback())
-    //
-    while chooser.shown() {
-        app::wait();
-    }
-    // User hit cancel?
-    if chooser.value(1).is_none() {
-        println!("(User hit 'Cancel')");
-        return;
-    }
-    // Print what the user picked
-    println!("--------------------");
-    println!("DIRECTORY: '{}'", chooser.directory().unwrap());
-    println!("    VALUE: '{}'", chooser.value(1).unwrap()); // value starts at 1!
-    println!("    COUNT: {} files selected", chooser.count());
-    // Multiple files? Show all of them
-    if chooser.count() > 1 {
-        for t in 1..=chooser.count() {
-            println!(" VALUE[{}]: '{}'", t, chooser.value(t).unwrap());
-        }
-    }
-    win.end();
-    win.show();
-    app.run().unwrap();
-}
-*/
