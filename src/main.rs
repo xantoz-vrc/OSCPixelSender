c3dbe3e0a114e307905cfe5e48dc9f7b0accda19
xantoz <xantoz@nowhere.com>
2024-12-29 21:03:29 +0100

    Now quantizes the image for display at least
    Using quantizr
    
    I guess from here it's time to start working on OSC things?
    Although perhaps we should take a crack at 4-bit grayscale first. I'm
    not sure if quantizr can handle that though...
    
    Also I realize that the 8-bit floats in VRC might represent a
    precision issue when we want to be sending u8s. We might end up having
    to use integers. Downside is then we have to make an animator to
    decode all that shit....

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use fltk::{app, frame::Frame, enums::FrameType, image::SharedImage, prelude::*, window::Window, group::*, button::Button, dialog};
+use fltk::{app, frame::Frame, enums::FrameType, image::*, enums::ColorDepth, prelude::*, window::Window, group::*, button::Button, dialog};
 use std::error::Error;
 use std::path::PathBuf;
+use std::iter::zip;
 
 fn get_file() -> Option<PathBuf> {
     let mut nfc = dialog::NativeFileChooser::new(dialog::FileDialogType::BrowseFile);
@@ -25,6 +26,48 @@ fn get_file() -> Option<PathBuf> {
             dialog::NativeFileChooserAction::Cancelled => None,
         }
     }
+}
+
+fn sharedimage_to_bytes(image : &SharedImage) -> Result<(Vec<u8>, usize, usize), Box<dyn Error>>
+{
+    let bytes : Vec<u8> = image.to_rgb_image()?.convert(ColorDepth::Rgba8)?.to_rgb_data();
+    println!("bytes.len(): {}", bytes.len());
+    let width : usize = image.data_w().try_into()?;
+    let height : usize = image.data_h().try_into()?;
+
+    Ok((bytes, width, height))
+}
+
+// Make it palletted image and then we reconvert it back to RgbImage
+// for display (I haven't found a way to display paletted images directly in FLTK)
+//
+// TODO: Split this up into two functions, one which returns the
+// indexes+palette and another which turns indexes + palette into an
+// RGBImage for display
+fn quantize_image(bytes : &Vec<u8>, width : usize, height : usize) -> Result<RgbImage, Box<dyn Error>> {
+
+    let qimage = quantizr::Image::new(bytes, width, height)?;
+    let mut qopts = quantizr::Options::default();
+    qopts.set_max_colors(16)?;
+
+    let mut result = quantizr::QuantizeResult::quantize(&qimage, &qopts);
+    result.set_dithering_level(1.0)?;
+
+    let mut indexes = vec![0u8; width*height];
+    result.remap_image(&qimage, indexes.as_mut_slice())?;
+
+    let palette = result.get_palette();
+
+    // -------------------- cut here --------------------
+
+    // Turn the quantized thing back into RGB for display
+    let mut fb: Vec<u8> = vec![0u8; width * height * 4];
+    for (index, pixel) in zip(indexes, fb.chunks_exact_mut(4)) {
+        let c : quantizr::Color = palette.entries[index as usize];
+        pixel.copy_from_slice(&[c.r, c.g, c.b, c.a]);
+    }
+
+    Ok(RgbImage::new(&fb, width as i32, height as i32, ColorDepth::Rgba8)?)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -75,9 +118,29 @@ fn main() -> Result<(), Box<dyn Error>> {
                     image.scale(256, 256, true, true);
                     println!("(after scale) w,h: {},{}", image.width(), image.height());
 
-                    fr.set_image(Some(image));
+                    let bresult = sharedimage_to_bytes(&image);
+                    let Ok((bytes, width, height)) = bresult else {
+                        let msg = format!("sharedimage_to_bytes failed: {bresult:?}");
+                        eprintln!("{}", msg);
+                        dialog::alert_default(&msg);
+                        return;
+                    };
+
+                    let qresult = quantize_image(&bytes, width, height);
+                    let Ok(rgbimage) = qresult else {
+                        let msg = format!("Quantization failed: {qresult:?}");
+                        eprintln!("{}", msg);
+                        dialog::alert_default(&msg);
+                        return;
+                    };
+
+                    fr.set_image(Some(rgbimage));
                     fr.set_label(&path.to_string_lossy());
                     fr.changed();
+
+                    // fr.set_image(Some(image));
+                    // fr.set_label(&path.to_string_lossy());
+                    // fr.changed();
 
                     // fr.set_image_scaled(Some(image));
                     // fr.set_label(path.to_string_lossy());
